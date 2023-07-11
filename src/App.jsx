44b576fb0794e10908d6d06fd1ee60e1b11d64e3
Informatica Departamento <informatica@santojanni.gcba.gov.ar>
2023-07-11 16:01:58 -0300

    modificamos app route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import RootProvider from "./core/providers/RootProvider";
 import { RouterProvider } from "react-router-dom";
-import { AppRouter } from './core/router/appRouter';
+import { AppRouter } from "../src/core/router/AppRouter";
 import HomeView from './pages/home/views/HomeView';
 
 function App() {
