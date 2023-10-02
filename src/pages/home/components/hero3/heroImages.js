426e1a4da5c105246fcc61325eeeb8cf7766106f
titojp <titojp2002@hotmail.com>
2023-10-02 19:18:36 -0300

    changing pictures

--- src/pages/home/components/hero3/heroImages.js
+++ src/pages/home/components/hero3/heroImages.js
@@ -1,30 +1,25 @@
-import image1 from "../../../../assets/images/hero_selection/hostel_frente_16_9.jpg"
-import image2 from "../../../../assets/images/hero_selection/people_16_9.jpg"
-import image3 from "../../../../assets/images/hero_selection/bed_towels.jpg"
-import image4 from "../../../../assets/images/hero_selection/hostel_frente_4_3.jpg"
-import image5 from "../../../../assets/images/hero_selection/hostel_frente_jp_16_9.jpg"
+// import image1 from "../../../../assets/images/hero_selection/hostel_frente_16_9.jpg"
+import image1 from "../../../../assets/images/hero_selection/hostel_frente_16_9_c.jpg"
+import image2 from "../../../../assets/images/hero_selection/bed_towels.jpg"
+import image3 from "../../../../assets/images/hero_selection/hostel_frente_4_3.jpg"
+// import image4 from "../../../../assets/images/hero_selection/hostel_frente_jp_16_9.jpg"
+
 
 
 export const images16x9 = [
     {
-        imageURL: image5,
+        imageURL: image1,
         imageText: "Estamos en el corazón de San Telmo - Buenos Aires"
     },
-
-    // {
-    //     imageURL: image2,
-    //     imageText: "Viví una aventura inolvidable!"
-    // },
-
     {
-        imageURL: image3,
+        imageURL: image2,
         imageText: "Nuestro hostel, tu hogar lejos de casa"
     }
 ]
 
 export const images4x3 = [
     {
-        imageURL: image4,
+        imageURL: image3,
         imageText: "Estamos en el corazón de San Telmo - Buenos Aires"
     }
 ]
